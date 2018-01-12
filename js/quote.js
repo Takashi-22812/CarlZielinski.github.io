5812ece13f17c04e703604d29b69c664aa51c0a0
Carl Zielinski <carl.zielinski92@gmail.com>
2018-01-11 17:40:49 -0700

    Initial Build
    Transfers files from original repo into Live Build 1

---
+++ js/quote.js
@@ -0,0 +1,159 @@
+var myQuotes =
+["If you know the enemy and know yourself, you need not fear the result of a hundred battles. If you know yourself but not the enemy, for every victory gained you will also suffer a defeat. If you know neither the enemy nor yourself, you will succumb in every battle.",
+ "When the debate is over, slander becomes the tool of the loser.",
+ "In the midst of chaos, there is also opportunity",
+ "An unexamined life is not worth living.",
+ "All mankind... being all equal and independent, no one ought to harm another in his life, health, liberty or possessions.",
+ "New opinions are always suspected, and usually opposed, without any other reason but because they are not common.",
+ "There is more stupidity than hydrogen in the universe, and it has a longer shelf life.",
+ "Rock journalism is people who can't write interviewing people who can't talk for people who can't read.",
+ "There is only one way to avoid criticism: do nothing, say nothing, and be nothing.",
+ "Insanity: doing the same thing over and over again and expecting different results.",
+ "Try not to become a man of success, but rather try to become a man of value.",
+ "They asked me how well I understood theoretical physics. I said I had a theoretical degree in physics. They said welcome aboard.",
+ "No, man. I know exactly what I'm doing. I just don't know what effect it's going to have. Over there controls power in this building. That station has readouts on the computer network. That big knob there makes a crazy noise. Sparks come out of that slot if you put stuff in it. And I'm learning more every day.",
+ "Truth is, the game was rigged from the start.",
+ "Patrolling the Mojave almost makes you wish for a nuclear winter.",
+ "A man chooses. A slave obeys.",
+ "NORMIES GET OUT REEEEEEEEEEEEEEE",
+ "REEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE",
+ "My name is Jeff.",
+ "[Dr. Pepper] - It's an intellectual drink for the chosen ones.",
+ "There is no end though there is a start in space. — Infinity. It has own power, it ruins, and it goes though there is a start also in the star. — Finite. Only the person who was wisdom can read the most foolish one from the history. The fish that lives in the sea doesn't know the world in the land. It also ruins and goes if they have wisdom. It is funnier that man exceeds the speed of light than fish start living in the land. It can be said that this is an final ultimatum from the god to the people who can fight.",
+ "The only thing we have to fear is fear itself.",
+ "Democracy cannot succeed unless those who express their choice are prepared to choose wisely. The real safeguard of democracy, therefore, is education.",
+ "Success is not final, failure is not fatal: it is the courage to continue that counts.",
+ "History is written by the victors.",
+ "I'd just like to interject for a moment. What you’re referring to as Linux, is in fact, GNU/Linux, or as I’ve recently taken to calling it, GNU plus Linux. Linux is not an operating system unto itself, but rather another free component of a fully functioning GNU system made useful by the GNU corelibs, shell utilities and vital system components comprising a full OS as defined by POSIX.",
+ "Isn't it ironic that the proprietary software developers call us communists? We are the ones who have provided for a free market, where they allow only monopoly. ...if the users chooses this proprietary software package, he then falls into this monopoly for support ... the only way to escape from monopoly is to escape from proprietary software, and that is what the free software movement is all about.",
+ "Free software permits students to learn how software works. Some students, on reaching their teens, want to learn everything there is to know about their computer and its software. They are intensely curious to read the source code of the programs that they use every day. To learn to write good code, students need to read lots of code and write lots of code. They need to read and understand real programs that people really use. Only free software permits this.",
+ "A computer would deserve to be called intelligent if it could deceive a human into believing that it was human.",
+ "No system of mass surveillance has existed in any society that we know of to this point that has not been abused.",
+ "Being a patriot doesn't mean prioritizing service to government above all else. Being a patriot means knowing when to protect your country, knowing when to protect your Constitution, knowing when to protect your countrymen, from the violations of and encroachments of adversaries. And those adversaries don't have to be foreign countries.",
+ "Uh, listen Jet. You said 'bell peppers and beef.'' There's no beef in here. So you wouldn't really call it 'bell peppers and beef,'' now would you?",
+ "This is real mystic and all, but uh, do you have anything to eat here?",
+ "You're tense, I'm calm. You apply excessive force and I control that force through fluid motion. That means relaxing the whole body so it can react instantly without resistance - no, without thought. Do you see now? It means becoming like clear water.",
+ "See You Space Cowboy...",
+ "I'll be the roundabout / The words will make you out 'n' out / I spend the day your way / Call it morning driving through the sound and in and out the valley...",
+ "Who controls the past controls the future. Who controls the present controls the past.",
+ "We know that no one ever seizes power with the intention of relinquishing it.",
+ "Power is not a means; it is an end. One does not establish a dictatorship in order to safeguard a revolution; one makes the revolution in order to establish the dictatorship. The object of persecution is persecution. The object of torture is torture. The object of power is power.",
+ "Momentum, a function of mass and velocity, is conserved between portals. In layman's terms, speedy thing goes in, speedy thing comes out.",
+ "YOU MUST CONSTRUCT ADDITIONAL PYLONS",
+ "NOT ENOUGH MINERALS",
+ "YOU REQUIRE MORE VESAPANE GAS",
+ "PAGE RESTRICTED"
+];
+
+var myQuotesAuthors =
+["Sun Tzu, The Art of War",
+ "Sun Tzu, The Art of War",
+ "Socrates",
+ "Socrates",
+ "John Locke",
+ "John Locke",
+ "Frank Zappa",
+ "Frank Zappa",
+ "Aristotle",
+ "Albert Einstein",
+ "Albert Einstein",
+ "Fantastic, Fallout: New Vegas",
+ "Fantastic, Fallout: New Vegas",
+ "Benny, Fallout: New Vegas",
+ "NCR Soldier, Fallout: New Vegas",
+ "Andrew Ryan, Bioshock",
+ "4chan",
+ "4chan",
+ "Greg Jenko, 22 Jump Street",
+ "Rintaro Okabe, Steins;Gate",
+ "Intro Text, Steins;Gate",
+ "Franklin D. Roosevelt",
+ "Franklin D. Roosevelt",
+ "Winston Churchill",
+ "Winston Churchill",
+ "Richard Stallman",
+ "Richard Stallman",
+ "Richard Stallman",
+ "Alan Turing",
+ "Edward Snowden",
+ "Edward Snowden",
+ "Spike Spiegel, Cowboy Bebop",
+ "Spike Spiegel, Cowboy Bebop",
+ "Spike Spiegel, Cowboy Bebop",
+ "Cowboy Bebop",
+ "Yes - Roundabout",
+ "George Orwell, 1984",
+ "George Orwell, 1984",
+ "George Orwell, 1984",
+ "GLaDOS, Portal",
+ "Starcraft",
+ "Starcraft",
+ "Starcraft",
+ "Millennium High School"
+];
+
+var randomQuote = myQuotes[Math.floor(Math.random() * myQuotes.length)];
+
+function randomQuoteInserter()
+{
+	document.getElementById("quotes").innerHTML = "“" + randomQuote + "”";
+}
+
+randomQuoteInserter();
+
+function authorOfRandomQuote(myQuotes)
+{
+	return myQuotes == randomQuote;
+}
+
+function authorOfRandomQuoteInserter()
+{
+	document.getElementById("quoteAuthor").innerHTML = "-" + myQuotesAuthors[myQuotes.findIndex(authorOfRandomQuote)];
+}
+
+authorOfRandomQuoteInserter();
+
+var myImages =
+["img/headimg1.jpg", "img/headimg2.jpg", "img/headimg3.jpg"]
+
+var randomImg = myImages[Math.floor(Math.random() * myImages.length)];
+
+function randomImgInserter()
+{
+	document.getElementById("HomepageImage").innerHTML = "<img src=" + "'" + randomImg + "'" + "'" + "height='900'" + "width='450'" + "title='Some image that has to do with computers'" + "alt='Some image that has to do with computers'" + ">";
+}
+
+function properCreditMaker()
+{
+	if(randomImg == "img/headimg1.jpg")
+	{
+		document.getElementById("ImageCredits2").innerHTML = "";
+		document.getElementById("ImageCredits3").innerHTML = "";
+	}
+	else if(randomImg == "img/headimg2.jpg")
+	{
+		document.getElementById("ImageCredits1").innerHTML = "";
+		document.getElementById("ImageCredits3").innerHTML = "";
+	}
+	else
+	{
+		document.getElementById("ImageCredits1").innerHTML = "";
+		document.getElementById("ImageCredits2").innerHTML = "";
+	}
+}
+
+properCreditMaker();
+randomImgInserter();
+
+//function darkToBright()
+//{
+//	document.body.style.backgroundColor = "white";
+//	document.getElementByClassName("paraHead").style.color = "black";
+//}
+
+function generateNewQuote()
+{
+	randomQuote = myQuotes[Math.floor(Math.random() * myQuotes.length)];
+	randomQuoteInserter();
+	authorOfRandomQuoteInserter();
+}
